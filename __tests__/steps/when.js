bfa02631dfb3da226d0ca91c9ef4842f287ed6f0
Josh Rogan <joshrogan93@gmail.com>
2024-02-04 22:09:10 +0000

    4 Add acceptance test for getMyProfile query

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -3,6 +3,7 @@ const AWS = require("aws-sdk");
 const fs = require("fs");
 const velocityMapper = require("amplify-appsync-simulator/lib/velocity/value-mapper/mapper");
 const velocityTemplate = require("amplify-velocity-template");
+const GraphQL = require("../lib/graphql");
 
 const we_invoke_confirmUserSignup = async (username, name, email) => {
   const handler = require("../../functions/confirm-user-signup").handler;
@@ -20,8 +21,8 @@ const we_invoke_confirmUserSignup = async (username, name, email) => {
         "cognito:email_alias": email,
         "cognito:user_status": "CONFIRMED",
         email_verified: "false",
-        name,
-        email,
+        name: name,
+        email: email,
       },
     },
     response: {},
@@ -71,12 +72,45 @@ const we_invoke_an_appsync_template = (templatePath, context) => {
     valueMapper: velocityMapper.map,
     escape: false,
   });
-
   return JSON.parse(compiler.render(context));
+};
+
+const a_user_calls_getMyProfile = async (user) => {
+  const getMyProfile = `query getMyProfile {
+    getMyProfile {
+      backgroundImageUrl
+      bio
+      birthdate
+      createdAt
+      followersCount
+      followingCount
+      id
+      imageUrl
+      likesCounts
+      location
+      name
+      screenName
+      tweetsCount
+      website
+    }
+  }`;
+
+  const data = await GraphQL(
+    process.env.API_URL,
+    getMyProfile,
+    {},
+    user.accessToken
+  );
+  const profile = data.getMyProfile;
+
+  console.log(`[${user.username}] - fetched profile`);
+
+  return profile;
 };
 
 module.exports = {
   we_invoke_confirmUserSignup,
-  we_invoke_an_appsync_template,
   a_user_signs_up,
+  we_invoke_an_appsync_template,
+  a_user_calls_getMyProfile,
 };
